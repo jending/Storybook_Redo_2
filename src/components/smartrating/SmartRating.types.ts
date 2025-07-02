007b38be4c2060c51416b72da43f4dc0ccd96e50
unknown <jenna.heidinger@gmail.com>
2025-07-02 12:06:10 -0500

    added storybook

---
+++ src/components/smartrating/SmartRating.types.ts
@@ -0,0 +1,10 @@
+export interface SmartRatingProps {
+    testIdPrefix: string;
+    title?: string;
+    theme: "primary" | "secondary";
+    disabled?: boolean;
+    size?: "small" | "medium" | "large";
+  }
+
+// SmartRating.tsx
+const SmartRating: React.FC<SmartRatingProps> = (props) => {return null}
